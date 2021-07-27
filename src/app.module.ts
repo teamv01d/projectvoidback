40eac6e2e6a897df1e34b66e5bc4c1ebc99d1d2a
Ekrem Can DEVE <devecann@gmail.com>
2021-07-27 16:42:56 +0300

    fazla olan moduleler çıkarıldı

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import environment from './env/environment';
 import { UsersModule } from './users/users.module';
-import { CompanyModule } from './company/company.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     UsersModule,
     MongooseModule.forRoot(environment.mongoUrl),
-    CompanyModule,
-    AuthModule,
   ],
   controllers: [],
   providers: [],
