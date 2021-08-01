584c45bf88e87e9306809d3fb0ce4b8b56936319
Can DEVE <devecann@gmail.com>
2021-08-01 21:15:37 +0300

    find question subject

--- src/aptitude-test/aptitude-test.service.ts
+++ src/aptitude-test/aptitude-test.service.ts
@@ -3,20 +3,28 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { AptitudeTestQuestion } from 'src/entities/aptitude-test-question.entity';
 import { CreateAptitudeTestDto } from './dto/create-aptitude-test.dto';
+import { SubjectClass } from 'src/entities/subject.entity';
 
 @Injectable()
 export class AptitudeTestService {
   constructor(
     @InjectModel(AptitudeTestQuestion.name)
-    private readonly AptitudeTestQuestionModel: Model<AptitudeTestQuestion>,
+    private readonly aptitudeTestQuestionModel: Model<AptitudeTestQuestion>,
+    private readonly subjectModel: Model<SubjectClass>,
   ) {}
 
   findAll(): Promise<AptitudeTestQuestion[] | undefined> {
-    return this.AptitudeTestQuestionModel.find().exec();
+    return this.aptitudeTestQuestionModel.find().exec();
   }
 
-  createAptitudeQuestion(createAptitudeTestDto: CreateAptitudeTestDto) {
-    const question = new this.AptitudeTestQuestionModel(createAptitudeTestDto);
-    return question.save();
+  async findSubject(subject: string): Promise<SubjectClass | undefined> {
+    return this.subjectModel.findOne({ subject });
   }
+
+  async createAptitudeQuestion(createAptitudeTestDto: CreateAptitudeTestDto) {}
+
+  // createAptitudeQuestion(createAptitudeTestDto: CreateAptitudeTestDto) {
+  //   const question = new this.AptitudeTestQuestionModel(createAptitudeTestDto);
+  //   return question.save();
+  // }
 }
