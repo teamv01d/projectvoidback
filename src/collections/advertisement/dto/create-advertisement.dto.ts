8fac10b5f2b0dad443f426189068849edc794e35
Can DEVE <devecann@gmail.com>
2021-08-09 01:46:03 +0300

    updated dto

--- src/collections/advertisement/dto/create-advertisement.dto.ts
+++ src/collections/advertisement/dto/create-advertisement.dto.ts
@@ -1,21 +1,26 @@
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 import { ObjectId, SchemaTypes } from 'mongoose';
 
 export class CreateAdvertisementDto {
   companyID: ObjectId;
 
   @IsString()
+  @IsOptional()
   readonly advertisement_name: string;
 
   @IsString()
+  @IsOptional()
   readonly explanation: string;
 
   @IsString()
+  @IsOptional()
   readonly start_date: string;
 
   @IsString()
+  @IsOptional()
   readonly end_date: string;
 
   @IsString()
+  @IsOptional()
   readonly city: string;
 }
